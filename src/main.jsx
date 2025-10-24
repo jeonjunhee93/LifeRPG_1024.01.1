eccaa67bfbbf1465045f2fc4fa3d89a55b478650
jeonjunhee93 <lllllljz@naver.com>
2025-10-24 21:30:31 +0900

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <h1>Life RPG 시작!</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
